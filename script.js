ff709ac390ff9285ca829b38e5b493f0ad13fa65
Liz Thompson <thompsonel2011@gmail.com>
2021-04-27 22:12:04 -0400

    add class name to avoid confusion + center icon

--- script.js
+++ script.js
@@ -2,12 +2,14 @@
 // 1. Display current date at top of page - format: Thursday, September 5th
 // 2. Display time on left side of scheduler
 // 3. Get the current time
-// 4. Change colors based on past(gray)/present(red)/future (green)
+// 4. Change colors based on past(gray)/present(red)/future(green)
 // 5. Allow user to type text in time block and save data via save button
 
 // GLOBAL VARIABLES 
+var saveBtnEl = $('#saveBtn');
+var textInputEl = $('.textInput');
 
-// 1. Display the current date underneath header
+// 1. Display the current date underneath header DONE
 $("#currentDay").text(moment().format('dddd, MMMM Do'));
 
 // 2. Display time (9AM - 5PM) on left side of scheduler
